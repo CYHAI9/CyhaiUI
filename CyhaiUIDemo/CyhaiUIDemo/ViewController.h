095fac0cb23315d64fcde0beceaaa1211a409369
又是一个大叔 <3069126500@qq.com>
2016-12-23 16:17:17 +0800

    创建Demo

---
+++ CyhaiUIDemo/CyhaiUIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyhaiUIDemo
+//
+//  Created by 陈海哥 on 2016/12/23.
+//  Copyright © 2016年 陈海哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CyhaiUIDemo/CyhaiUIDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CyhaiUIDemo
+//
+//  Created by 陈海哥 on 2016/12/23.
+//  Copyright © 2016年 陈海哥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
